34941ecb643f4e11fd736a3dc46edc93d54409ee
elshadbbyv <babayevelshad09@gmail.com>
2021-12-21 18:07:15 +0400

    Happy Mini Family

--- src/main/java/iktlab/javaPro/j4Group/Projects/One/Pet.java
+++ src/main/java/iktlab/javaPro/j4Group/Projects/One/Pet.java
@@ -1,27 +1,89 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package iktlab.javaPro.j4Group.Projects.One;
 
-import static iktlab.javaPro.j4Group.Projects.One.Human.pet;
+import java.util.Arrays;
 
 public class Pet {
-    public String species;
-    public String  nickname;
-    public int age;
-    public int tricklevel;
-    public String[] habits = new String[5];
+    String species;
 
-    static void eat(){
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public void setTricklevel(int tricklevel) {
+        this.tricklevel = tricklevel;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public int getTricklevel() {
+        return tricklevel;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
+    String nickname;
+    byte age;
+    int tricklevel;
+    private String[] habits;
+
+    private void eat() {
         System.out.println("I am Eating");
     }
 
-    static void  respond(){
-
-        System.out.println("Hello, owner. I am "+ pet.nickname+". I miss you");
+    private void respond() {
+        System.out.printf("Hello, owner. I am %s. I miss you", Human.pet.nickname);
     }
-    static void foul(){
+
+    private void foul() {
         System.out.println("I need to cover it up");
     }
 
+    public String toString() {
+        return "Pet{species='" + this.species + '\'' + ", nickname='" + this.nickname + '\'' + ", age=" + this.age + ", tricklevel=" + this.tricklevel + ", habits=" + Arrays.toString(this.habits) + '}';
+    }
 
+    Pet() {
+    }
 
+    private Pet(String species, String nickname) {
+        this.species = species;
+        this.nickname = nickname;
+    }
 
+    Pet(String species, String nickname, byte age, byte tricklevel, String[] habits) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = age;
+        this.tricklevel = tricklevel;
+        this.habits = habits;
+    }
 }
